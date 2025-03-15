b658d232e36f9b7e4e2f24206a65046556e70d43
Charityo-o <charity.rininger@students.fhu.edu>
2025-03-15 17:49:16 -0500

    Most up to date version:
    sorting ceremony and start of the trivia section

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def sorting_ceremony():
     R = 0
     H = 0
     m = 0
-    print('Okay',name+', answer the following questions with "a", "b", "c", or "d".\nMake sure your answer is not capitalized.')
+    print('Okay',name+', answer the following questions with "a", "b", "c", or "d".\nDo ensure your response is not in capital letters.')
 
     print("\nquestion and answers \na: \nb: \nc: \nd:")
     q1 = input()
@@ -58,7 +58,7 @@ def sorting_ceremony():
     else:
         m +=1
     
-    print("\nWhat house would you like to be in?\na: Gryffindor\nb: Hufflepuff\nc: Ravenclaw\nd: Slytherin")
+    print("\nWhich house do you believe you belong to?\na: Gryffindor\nb: Hufflepuff\nc: Ravenclaw\nd: Slytherin")
     q5 = input()
     if q5 == 'a':
         tie = "Gryffindor"
@@ -92,15 +92,101 @@ def sorting_ceremony():
             print("Really, you couldn't pick one... I guess you're a muggle.")
         return name, tie
 
+def trivia(name):
+    print("Okay", name+", trivia time!")
+    points = 0
+    print("\nquestion and answers \na: \nb: \nc: \nd:")
+    q1 = input()
+    if q1 == 'a':
+        points += 1
 
+    print("\nquestion and answers \na: \nb: \nc: \nd:")
+    q2 = input()
+    if q2 == 'a':
+        points += 1
+
+    print("\nquestion and answers \na: \nb: \nc: \nd:")
+    q3 = input()
+    if q3 == 'a':
+        points += 1
+
+    print("\nquestion and answers \na: \nb: \nc: \nd:")
+    q4 = input()
+    if q4 == 'a':
+        points += 1
+
+        print("\nquestion and answers \na: \nb: \nc: \nd:")
+    q5 = input()
+    if q5 == 'a':
+        points += 1
+
+    print("\nquestion and answers \na: \nb: \nc: \nd:")
+    q6 = input()
+    if q6 == 'a':
+        points += 1
+
+    print("\nquestion and answers \na: \nb: \nc: \nd:")
+    q7 = input()
+    if q7 == 'a':
+        points += 1
+
+    print("\nquestion and answers \na: \nb: \nc: \nd:")
+    q8 = input()
+    if q8 == 'a':
+        points += 1
+
+    print("\nquestion and answers \na: \nb: \nc: \nd:")
+    q9 = input()
+    if q9 == 'a':
+        points += 1
+
+    print("\nquestion and answers \na: \nb: \nc: \nd:")
+    q10 = input()
+    if q10 == 'a':
+        points += 1
+
+    print("\nquestion and answers \na: \nb: \nc: \nd:")
+    q11 = input()
+    if q11 == 'a':
+        points += 1
+
+    print("\nquestion and answers \na: \nb: \nc: \nd:")
+    q12 = input()
+    if q12 == 'a':
+        points += 1
+
+    print("\nquestion and answers \na: \nb: \nc: \nd:")
+    q13 = input()
+    if q13 == 'a':
+        points += 1
+
+    print("\nquestion and answers \na: \nb: \nc: \nd:")
+    q14 = input()
+    if q14 == 'a':
+        points += 1
+
+    print("\nquestion and answers \na: \nb: \nc: \nd:")
+    q15 = input()
+    if q15 == 'a':
+        points += 1
+2
 
 if __name__ == '__main__':
-    num_users = int(input("How many new students are there this year? "))
-    print("Let's begin the sorting ceremony.")
+    num_users = int(input("How many fresh-faced young witches and wizards have joined our ranks this year? "))
+    print("Let the Sorting Ceremony commence!")
     user_dict = {}
     for i in range (num_users):
         name, house= sorting_ceremony()
         user_dict[name] = house
-        print(name, "you belong in", house+"!")
+        print(name, "you belong in...", house+"!")
+        print("Would the next young wizard please step forward!")
         print(user_dict)
+
+    print("Now that all are sorted, it's time for a test of wit! Answer wisely to earn points for the House Cup—may the best house triumph!")
+    
+    for key, value in user_dict.items():
+        trivia(key)
+
+
+
 
