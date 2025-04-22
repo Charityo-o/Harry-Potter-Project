be9f05b780d9e851a916943c1a3a4edd90fdc115
rosaroja777 <maria.ortiz@students.fhu.edu>
2025-04-21 19:35:51 -0500

    Update trivia_questions.py
    Change Question class name from trivia_questions.py to avoid confusion

--- trivia_questions.py
+++ trivia_questions.py
@@ -1,23 +1,23 @@
-class Question:
+class Quiz:
     def __init__(self, ask, answer):
         self.ask = ask 
         self.answer = answer
 
-q1 = Question("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
-q2 = Question("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
-q3 = Question("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
-q4 = Question("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
-q5 = Question("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
-q6 = Question("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
-q7 = Question("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
-q8 = Question("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
-q9 = Question("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
-q10 = Question("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
-q11 = Question("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
-q12 = Question("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
-q13 = Question("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
-q14 = Question("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
-q15 = Question("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
+q1 = Quiz("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
+q2 = Quiz("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
+q3 = Quiz("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
+q4 = Quiz("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
+q5 = Quiz("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
+q6 = Quiz("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
+q7 = Quiz("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
+q8 = Quiz("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
+q9 = Quiz("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
+q10 = Quiz("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
+q11 = Quiz("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
+q12 = Quiz("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
+q13 = Quiz("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
+q14 = Quiz("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
+q15 = Quiz("\nquestion and answers \na: \nb: \nc: \nd:", 'a')
 questions = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15]
 
 def trivia(name):
